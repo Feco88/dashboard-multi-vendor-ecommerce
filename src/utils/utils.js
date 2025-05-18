8779cd95e530da1bea11b630179963de50c970e1
Feco88 <fecaboy@gmail.com>
2025-05-18 19:52:52 +0200

    Io-socket-client

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,4 @@
+/*
 import io from 'socket.io-client'
 
 export const overrideStyle = {
@@ -8,3 +9,17 @@ export const overrideStyle = {
     alignItems: 'center'
 }
 export const socket = io('http://localhost:5000')
+*/
+import io from 'socket.io-client'
+
+const socketURL = process.env.REACT_APP_SOCKET_URL || 'http://localhost:5000'
+
+export const socket = io(socketURL)
+
+export const overrideStyle = {
+    display: 'flex',
+    margin: '0 auto',
+    height: '24px',
+    justifyContent: 'center',
+    alignItems: 'center'
+}
